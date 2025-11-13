0b090f9dfd6c7dcd7cf94669f2d79749ed42bf87
Arialdo Martini <arialdo.martini@gmail.com>
2025-11-13 07:57:09 +0100

    Code commented and removed, for exercise

--- src/FSTest/Session06/Internals/ApplicativeBuilder.fs
+++ src/FSTest/Session06/Internals/ApplicativeBuilder.fs
@@ -6,80 +6,81 @@ open Swensen.Unquote
 open Content
 open Result
 open Repositories
-
-let buildNote (number: NoteNumber) (account: Account) (content: string) (createdAt: DateTime) (noteType: NoteType) =
-    { number = number
-      account = account
-      content = content
-      createdAt = createdAt
-      noteType = noteType }
-
-
-let getNote (noteNumber: NoteNumber) =
-    let content = getContent noteNumber
-
-    let build content =
-        let number = parseNumber content.id
-        let account = parseAccount content.account
-        let contentText = pure' content.text
-        let createdAt = parseDate content.created
-        let noteType = parseNoteType content.ofType
-
-        buildNote <!> number <*> account <*> contentText <*> createdAt <*> noteType
-
-    content >>= build
-
-[<Fact>]
-let ``successfully builds an Account`` () =
-
-    let account = parseAccount "joe"
-
-    let expected =
-        Ok
-            { name = "Joe"
-              email = Email "joe.doe@example.com"
-              role = User }
-
-    test <@ account = expected @>
-
-    let account = parseAccount "unknown"
-
-    test <@ account = Failure [ "Account for 'unknown' could not be found" ] @>
-
-[<Fact>]
-let ``Successfully found Note`` () =
-
-    let note = getNote (NoteNumber 42)
-
-    let expected =
-        Ok
-            { number = NoteNumber 42
-              account =
-                { name = "Joe"
-                  email = Email "joe.doe@example.com"
-                  role = User }
-              content = "This is note 42"
-              createdAt = DateTime(2025, 12, 11)
-              noteType = Secrets }
-
-    test <@ note = expected @>
-
-[<Fact>]
-let ``Not found Note`` () =
-
-    let note = getNote (NoteNumber 12345)
-
-    test <@ note = Failure [ "Note 12345 does not exist" ] @>
-
-[<Fact>]
-let ``Accumulation of errors`` () =
-
-    let note = getNote (NoteNumber 99)
-
-    test
-        <@
-            note = Failure
-                [ "'s�cret' is not a valid note type"
-                  "25-12- is not a valid date"
-                  "Account for 'jan' could not be found" ]
-        @>
+//
+// let buildNote (number: NoteNumber) (account: Account) (content: string) (createdAt: DateTime) (noteType: NoteType) =
+//     { number = number
+//       account = account
+//       content = content
+//       createdAt = createdAt
+//       noteType = noteType }
+//
+//
+// let getNote (noteNumber: NoteNumber) =
+//
+//     let build content =
+//         let number = parseNumber content.id
+//         let account = parseAccount content.account
+//         let contentText = pure' content.text
+//         let createdAt = parseDate content.created
+//         let noteType = parseNoteType content.ofType
+//
+//         buildNote <!> number <*> account <*> contentText <*> createdAt <*> noteType
+//
+//     let content = getContent noteNumber
+//
+//     build =<< content
+//
+// [<Fact>]
+// let ``successfully builds an Account`` () =
+//
+//     let account = parseAccount "joe"
+//
+//     let expected =
+//         Ok
+//             { name = "Joe"
+//               email = Email "joe.doe@example.com"
+//               role = User }
+//
+//     test <@ account = expected @>
+//
+//     let account = parseAccount "unknown"
+//
+//     test <@ account = Failure [ "Account for 'unknown' could not be found" ] @>
+//
+// [<Fact>]
+// let ``Successfully found Note`` () =
+//
+//     let note = getNote (NoteNumber 42)
+//
+//     let expected =
+//         Ok
+//             { number = NoteNumber 42
+//               account =
+//                 { name = "Joe"
+//                   email = Email "joe.doe@example.com"
+//                   role = User }
+//               content = "This is note 42"
+//               createdAt = DateTime(2025, 12, 11)
+//               noteType = Secrets }
+//
+//     test <@ note = expected @>
+//
+// [<Fact>]
+// let ``Not found Note`` () =
+//
+//     let note = getNote (NoteNumber 12345)
+//
+//     test <@ note = Failure [ "Note 12345 does not exist" ] @>
+//
+// [<Fact>]
+// let ``Accumulation of errors`` () =
+//
+//     let note = getNote (NoteNumber 99)
+//
+//     test
+//         <@
+//             note = Failure
+//                 [ "'s�cret' is not a valid note type"
+//                   "25-12- is not a valid date"
+//                   "Account for 'jan' could not be found" ]
+//         @>
--- src/FSTest/Session06/Internals/Result.fs
+++ src/FSTest/Session06/Internals/Result.fs
@@ -1,36 +1,22 @@
 module FSTest.Session06.Internals.Result
-
-type Result<'success> =
-    | Ok of 'success
-    | Failure of string list
-
-
-let pure' a = Ok a
-
-let map f (a: 'a Result) =
-    match a with
-    | Ok a -> Ok (f a)
-    | Failure e -> Failure e
-
-let (<!>) = map
-
-let ap (f: ('a -> 'b) Result) (a: 'a Result) =
-    match f with
-    | Ok fOk ->
-        match a with
-        | Ok aValue -> Ok (fOk aValue)
-        | Failure aErr -> Failure aErr
-    | Failure fErrors ->
-        match a with
-        | Ok _ -> Failure fErrors
-        | Failure aErrors -> Failure (aErrors @ fErrors)
-
-
-let (<*>) = ap
-
-let bind (a: 'a Result) (f: 'a -> 'b Result) =
-    match a with
-    | Ok a -> f a
-    | Failure errors -> Failure errors
-
-let (>>=) = bind
+//
+// type Result<'success> =
+//
+//
+//
+// let pure' a = failwith "Not yet implemented"
+//
+// let map f a = failwith "Not yet implemented"
+//
+// let (<!>) a b = failwith "Not yet implemented"
+//
+// let ap (f) (a) =
+//     failwith "Not yet implemented"
+//
+// let (<*>) = ap
+//
+// let bind a f = failwith "Not yet implemented"
+//
+// let (>>=) a b = failwith "Not yet implemented"
+//
+// let (=<<) a b = failwith "Not yet implemented"
