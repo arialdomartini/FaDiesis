cd4074e648994acfbf0fd5c6ff4889c1f49311d0
Arialdo Martini <arialdo.martini@gmail.com>
2025-12-16 17:48:13 +0100

    WithCount

---
+++ src/FSTest/Session11/StateMonad/IndexingTrees/WithCount.fs
@@ -0,0 +1,39 @@
+module FSTest.Session11.StateMonad.IndexingTrees.WithCount
+
+
+type Tree<'a> =
+    | Leaf of 'a
+    | Node of Tree<'a> * Tree<'a>
+
+let rec map f tree =
+    match tree with
+    | Leaf v -> Leaf(f v)
+    | Node(l, r) -> Node(map f l, map f r)
+
+
+open Xunit
+open Swensen.Unquote
+
+
+type WithCount<'v> = WithCount of (int -> 'v * int)
+
+let run (WithCount f) count = f count
+
+let rec index =
+    function
+    | Leaf v -> WithCount (fun count -> (Leaf (v, count), count+1))
+    | Node(l, r) -> WithCount (
+        fun count ->
+            let li, lc = run (index l) count
+            let ri, rc = run (index r) lc
+
+            (Node (li, ri), rc))
+
+
+[<Fact>]
+let ``indexes a tree`` () =
+    let tree = Node(Leaf "one", Node(Leaf "two", Leaf "three"))
+
+    let indexed, _ = run (index tree) 1
+
+    test <@ indexed = Node(Leaf ("one", 1), Node(Leaf ("two", 2), Leaf ("three", 3))) @>
